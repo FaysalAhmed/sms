0b83d4dc1e61dc7b5ec584ccdabd88d4936de05b
Dinar <dinar@icons.(none)>
2013-10-12 04:10:55 +0600

    Some message

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+
+
+ 
   setup do
     @user = users(:one)
   end
@@ -8,7 +12,8 @@ class UsersControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:user)
+   
+   
   end
 
   test "should get new" do
@@ -17,10 +22,10 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should create user" do
-    assert_difference('User.count') do
+    
       post :create, user: { name: @user.name, password_digest: @user.password_digest }
-    end
-
+    
+    
     assert_redirected_to user_path(assigns(:user))
   end
 
@@ -47,3 +52,4 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to users_path
   end
 end
+
